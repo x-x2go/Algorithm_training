a66a4276f32b100576de0163b958becbe125e503
x-x2go <x-x2go>
2021-03-05 17:47:26 +0900

    거스름돈💰

--- Programmers/거스름돈.js
+++ Programmers/거스름돈.js
@@ -1,1 +1,17 @@
-console.log();
+const n = 5;
+const money = [1,2,5];
+
+console.log(solution(n, money));
+
+function solution(n, money) {
+
+    const check = Array.apply(null, new Array(n+1)).map((x, i)=>i % money[0] ? 0 : 1);
+
+    for(let i = 1; i < money.length; i++){
+        for(let j = money[i]; j <= n; j++){
+            check[j] += check[j - money[i]];
+        }
+    }
+ 
+    return check.pop() % 1000000007;
+}
